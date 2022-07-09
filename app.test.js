25076939c3cbff1490dbae72e8373d3ee723f0b9
IsuruAkalanka <isurucuma@gmail.com>
2022-07-09 21:31:02 +0530

    initial commit

---
+++ app.test.js
@@ -0,0 +1,4 @@
+const request = require("supertest");
+const app = require("./app");
+
+it("should pass this", () => {});
---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+    return <div className="App">Academic Calendar Management Application</div>;
+}
+
+export default App;
