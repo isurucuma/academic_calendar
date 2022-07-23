8afce45a99c73ada0380e161a0706f042c26c3c7
IsuruAkalanka <isurucuma@gmail.com>
2022-07-23 08:20:20 +0530

    2022/07/23 8:20 am

--- routes/events.js
+++ routes/events.js
@@ -3,89 +3,93 @@ var express = require("express");
 var router = express.Router();
 
 const Event = new mongoose.model(
-  "events",
-  new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-    },
-    startDate: {
-      type: Date,
-      required: true,
-    },
-    endDate: {
-      type: Date,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    batch: {
-      type: String,
-    },
-  })
+    "events",
+    new mongoose.Schema({
+        title: {
+            type: String,
+            required: true,
+        },
+        startDate: {
+            type: Date,
+            required: true,
+        },
+        endDate: {
+            type: Date,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        batch: {
+            type: String,
+        },
+    })
 );
 
 /* POST event. */
 router.post("/create", async (req, res, next) => {
-  try {
-    let event = new Event({
-      title: req.body.title,
-      startDate: req.body.startDate,
-      endDate: req.body.endDate,
-      description: req.body.description,
-      batch: req.body.batch,
-    });
-    event = await Event.create(event);
-    res.send(event).status(200);
-  } catch (error) {
-    res.send(error.message).status(400);
-  }
+    try {
+        let event = new Event({
+            title: req.body.title,
+            startDate: req.body.startDate,
+            endDate: req.body.endDate,
+            description: req.body.description,
+            batch: req.body.batch,
+        });
+        event = await Event.create(event);
+        res.send(event).status(200);
+    } catch (error) {
+        res.send(error.message).status(400);
+    }
 });
 
 /* GET events listing. */
 router.get("/", async (req, res, next) => {
-  try {
-    const events = await Event.find();
-    res.send(events);
-  } catch (error) {
-    res.send(error.message).status(400);
-  }
+    try {
+        const events = await Event.find();
+        res.send(events);
+    } catch (error) {
+        res.send(error.message).status(400);
+    }
 });
 
 /* update event listing. */
 router.put("/update/:id", async (req, res, next) => {
-  try {
-    const entry = await Event.findById(req.params.id);
+    try {
+        const entry = await Event.findById(req.params.id);
 
-    if (!entry) {
-      res.send("Event doesn't exist").status(400);
-    } else {
-      const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-      });
-      res.send(event).status(200);
+        if (!entry) {
+            res.send("Event doesn't exist").status(400);
+        } else {
+            const event = await Event.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                {
+                    new: true,
+                }
+            );
+            res.send(event).status(200);
+        }
+    } catch (error) {
+        res.send(error.message).status(400);
     }
-  } catch (error) {
-    res.send(error.message).status(400);
-  }
 });
 
 /* delete event listing. */
 router.delete("/update/:id", async (req, res, next) => {
-  try {
-    const entry = await Event.findById(req.params.id);
+    try {
+        const entry = await Event.findById(req.params.id);
 
-    if (!entry) {
-      res.send("Event doesn't exist").status(400);
-    } else {
-      const event = await Event.findByIdAndRemove(req.params.id);
-      res.send(event).status(200);
+        if (!entry) {
+            res.send("Event doesn't exist").status(400);
+        } else {
+            const event = await Event.findByIdAndRemove(req.params.id);
+            res.send(event).status(200);
+        }
+    } catch (error) {
+        res.send(error.message).status(400);
     }
-  } catch (error) {
-    res.send(error.message).status(400);
-  }
 });
 
 module.exports = router;
